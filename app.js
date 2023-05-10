bff1f3c7831d5279ecc7b55d45d591f051460118
Xavi Guasch <xaviguasch@gmail.com>
2023-05-10 21:22:47 +0200

    toyed with mongodb cloud database connection

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-require('dotenv').config()
+const connectDB = require('./config/database')
+
+//Use .env file in config folder
+require('dotenv').config({ path: './config/.env' })
+
+const mongoConnect = require('./config/database')
 
 const app = express()
 
+connectDB()
+
 const PORT = process.env.port || 5000
+
+app.get('/test', (req, res, next) => {
+  res.json({ msg: 'welcome to the api' })
+})
 
 app.listen(PORT, () => console.log(`Listening on: ${PORT}`))
